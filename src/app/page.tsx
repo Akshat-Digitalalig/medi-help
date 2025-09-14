dabf34b87d6be30872f365630237b610d67b7330
Deepak Saraswat <deepaksaraswat@Deepaks-MacBook-Air.local>
2025-09-15 01:48:23 +0530

    minor commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         <div className='bg-[#240f6a] text-[#8c6df7] flex justify-around overflow-hidden items-center rounded-2xl h-48 w-full '>
           <div className='pl-4'>
             <h1 className='text-xl md:text-2xl xl:text-4xl leading-none font-bold'>Do You Want Free Consultant</h1>
-            <h1 className='text-xl leading-none md:text-3xl xl:text-6xl font-bold text-white'>With Top Doctors</h1>
+            <h1 className='text-xl leading-none md:text-3xl xl:text-6xl font-bold text-white'>With Top Doctors At Home</h1>
           </div>
           <Image src={'/Doctors.png'} alt="Free Consult" className='' height={290} width={490} />
         </div>
